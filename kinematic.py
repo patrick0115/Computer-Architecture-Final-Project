36f26883caf80e058dd7dd50d1ad3468d6585da6
patrick0115 <karta1770900@gmail.com>
2022-06-15 23:55:02 +0800

    add

---
+++ kinematic.py
@@ -0,0 +1,85 @@
+import math
+import numpy as np
+
+# Parameter
+a=[0.120 ,0.250, 0.260, 0 ,0 ,0 ]
+d=[0 ,0 ,0, 0, 0, 0, 0 ]
+alpha=[-90 ,0, 0 ,-90 ,90, 0]
+
+# Trigonometric functions
+rad = 180 / math.pi
+def sin(degree):
+	degree = degree / rad
+	sin = math.sin(degree)
+	return round(sin,15)
+
+def cos(degree):
+	degree = degree / rad
+	cos = math.cos(degree)
+	return round(cos,15)
+
+def tan(degree):
+	degree = degree / rad
+	tan = math.tan(degree)
+	return round(tan,15)
+
+def asin(x):
+	asin = math.asin(x) * rad
+	return  round(asin,14)
+
+def acos(x):
+	acos = math.acos(x) * rad
+	return  round(acos,13)
+
+def atan(x):
+	atan = math.atan(x) * rad
+	return  round(atan,16)
+
+# input
+theta=[90, 99 ,-119 ,-10 ,10, 0 ]
+
+# DH-model
+def dh(theta,alpha,d,a):
+    A= np.array([[cos(theta),-sin(theta)*cos(alpha),sin(theta)*sin(alpha),a*cos(theta)],
+        [sin(theta),cos(theta)*cos(alpha),-cos(theta)*sin(alpha),a*sin(theta)],[0,sin(alpha),cos(alpha),d],[0,0,0,1]])
+    # print(T)
+    return np.array(A)
+
+# calculate A
+A = np.empty((6, 4, 4)) 
+T = np.empty((4, 4))
+for i, item in enumerate(A):
+	A[i] = dh(theta[i], alpha[i], d[i], a[i])
+#	print('A[',i+1,']:')
+#	print(A[i])
+	
+	
+# write A in TXT
+for i in range(6):
+	file_name = 'A'+str(i+1)+'.txt'
+	f= open(file_name,'w')
+	for j in range(4):
+		for k in range(4):
+			f.write(str(A[i,j,k]))
+			f.write('\n')
+f.flush
+f.close
+
+# calculate T
+T=np.linalg.multi_dot([A[0],A[1],A[2],A[3],A[4],A[5]])
+
+f= open('ANS','w')
+for j in range(4):
+	for k in range(4):
+		f.write(str(T[j,k]))
+		f.write('\n')
+print('ANS:')
+print(T)
+
+
+
+
+
+
+
+
---
+++ kinematic_cuda.cu
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cuda_runtime.h>
+#include <math.h>
+#define BLOCK_SIZE 16
+
+__global__ static void matMultCUDA(const float* a, size_t lda, const float* b, size_t ldb, float* c, size_t ldc, int n)
+{
+	__shared__ float matA[BLOCK_SIZE][BLOCK_SIZE];
+	__shared__ float matB[BLOCK_SIZE][BLOCK_SIZE];
+	const int tidc = threadIdx.x;
+	const int tidr = threadIdx.y;
+	const int bidc = blockIdx.x * BLOCK_SIZE;
+	const int bidr = blockIdx.y * BLOCK_SIZE;
+	int i, j;
+
+	float results = 0;
+	float comp = 0;
+
+	for(j = 0; j < n; j += BLOCK_SIZE) {
+		matA[tidr][tidc] = a[(tidr + bidr) * lda + tidc + j];
+		matB[tidr][tidc] = b[(tidr + j) * ldb + tidc + bidc];
+
+		__syncthreads();
+
+		for(i = 0; i < BLOCK_SIZE; i++) {
+			float t;
+			comp -= matA[tidr][i] * matB[i][tidc];
+			t = results - comp;
+			comp = (t - results) + comp;
+			results = t;
+		}
+
+		__syncthreads();
+	}
+
+	c[(tidr + bidr) * ldc + tidc + bidc] = results;
+}
+
+
+
+clock_t matmultCUDA(const float* a, int lda, const float* b, int ldb, float* c, int ldc, int n)
+{
+	float *ac, *bc, *cc;
+	clock_t start, end;
+	size_t pitch_a, pitch_b, pitch_c;
+	int newn = ((n + BLOCK_SIZE - 1) / BLOCK_SIZE) * BLOCK_SIZE;
+
+	start = clock();
+	cudaMallocPitch((void**) &ac, &pitch_a, sizeof(float) * newn, newn);
+	cudaMallocPitch((void**) &bc, &pitch_b, sizeof(float) * newn, newn);
+	cudaMallocPitch((void**) &cc, &pitch_c, sizeof(float) * newn, newn);
+
+	cudaMemset(ac, 0, pitch_a * newn);
+	cudaMemset(bc, 0, pitch_b * newn);
+
+	cudaMemcpy2D(ac, pitch_a, a, sizeof(float) * lda, sizeof(float) * n, n, cudaMemcpyHostToDevice);
+	cudaMemcpy2D(bc, pitch_b, b, sizeof(float) * ldb, sizeof(float) * n, n, cudaMemcpyHostToDevice);
+
+	int bx = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
+	dim3 blocks(bx, bx);
+	dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+	matMultCUDA<<<blocks, threads>>>(ac, pitch_a / sizeof(float), bc, pitch_b / sizeof(float), cc, pitch_c / sizeof(float), n);
+
+	cudaMemcpy2D(c, sizeof(float) * ldc, cc, pitch_c, sizeof(float) * n, n, cudaMemcpyDeviceToHost);
+
+	cudaFree(ac);
+	cudaFree(bc);
+	cudaFree(cc);
+
+	end = clock();
+
+	return end - start;
+}
+
+void matmult(const float* a, int lda, const float* b, int ldb, float* ans, int ldc, int n)
+{
+	int i, j, k;
+
+	for(i = 0; i < n; i++) {
+		for(j = 0; j < n; j++) {
+			double t = 0;
+			for(k = 0; k < n; k++) {
+				t += a[i * lda + k] * b[k * ldb + j];
+			}
+			ans[i * ldc + j] = t;
+		}
+	}
+}
+
+bool InitCUDA()
+{
+	int count;
+
+	cudaGetDeviceCount(&count);
+	if(count == 0) {
+		fprintf(stderr, "There is no device.\n");
+		return false;
+	}
+
+	int i;
+	for(i = 0; i < count; i++) {
+		cudaDeviceProp prop;
+		if(cudaGetDeviceProperties(&prop, i) == cudaSuccess) {
+			if(prop.major >= 1) {
+				break;
+			}
+		}
+	}
+
+	if(i == count) {
+		fprintf(stderr, "There is no device supporting CUDA 1.x.\n");
+		return false;
+	}
+
+	cudaSetDevice(i);
+
+	return true;
+}
+
+void ReadData(char *name, float* matrix){
+	int j = 0;
+	float data;
+	FILE *fp = fopen(name, "r");
+	while (fscanf(fp,"%f",&data) != EOF && j<16) {
+			matrix[j]=data;
+//			printf("data=%.6f a=%.6f\n",data,matrix[j]);
+			j=j+1;		
+		}
+	fclose(fp);
+}
+
+int main()
+{
+	
+	float *a, *b, *c, *d, *e, *f ,*ansGPU ;
+	int n = sqrt(BLOCK_SIZE) ;
+
+	if(!InitCUDA()) {
+		return 0;
+	}
+
+	a = (float*) malloc(sizeof(float) * n * n);
+	b = (float*) malloc(sizeof(float) * n * n);
+	c = (float*) malloc(sizeof(float) * n * n);	
+	d = (float*) malloc(sizeof(float) * n * n);
+	e = (float*) malloc(sizeof(float) * n * n);
+	f = (float*) malloc(sizeof(float) * n * n);
+	ansGPU = (float*) malloc(sizeof(float) * n * n);
+	//ansCPU = (float*) malloc(sizeof(float) * n * n);
+	srand(0);    
+	
+	int i=0;
+	int j=0;
+	
+	ReadData("A1.txt", a);
+	ReadData("A2.txt", b);
+	ReadData("A3.txt", c);
+	ReadData("A4.txt", d);
+	ReadData("A5.txt", e);
+	ReadData("A6.txt", f);
+
+	clock_t time1 = matmultCUDA(a, n, b, n, ansGPU, n, n);
+	clock_t time2 = matmultCUDA(ansGPU, n, c, n, ansGPU, n, n);
+	clock_t time3 = matmultCUDA(ansGPU, n, d, n, ansGPU, n, n);
+	clock_t time4 = matmultCUDA(ansGPU, n, e, n, ansGPU, n, n);
+	clock_t time5 = matmultCUDA(ansGPU, n, f, n, ansGPU, n, n);
+	clock_t time =time1+ time2+time3+time4+time5;
+	matmult(a, n, b, n, d, n, n);
+	
+	printf("Answer of kinematic from GPU:\n");
+	printf("Cartesian point:\n");
+	printf("|    n     |    o     |     a    |     p    |\n");
+	for(i = 0; i < n; i++) {
+		printf("|");
+		for(j = 0; j < n; j++) {
+			printf("%10f|",ansGPU[i * n + j]);
+		}
+		printf("\n");
+	}
+
+	double sec = (double) time / CLOCKS_PER_SEC;
+	printf("Time used: %lf   (%lf GFLOPS)\n", sec, 2.0 * n * n * n / (sec * 1E9));
+
+	free(a);
+	free(b);
+	free(c);	
+	free(d);
+	free(e);
+	free(f);
+	free(ansGPU);
+	return 0;
+}
